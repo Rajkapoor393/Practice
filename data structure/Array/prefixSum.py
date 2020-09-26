b55abc786c3b35bcd0efbe9d67bd53031d12c6fd
Raj Kapoor <36943290+Rajkapoor393@users.noreply.github.com>
2020-09-26 23:32:41 +0530

    Add files via upload

---
+++ data structure/Array/prefixSum.py
@@ -0,0 +1,23 @@
+# Python program for implementing prefix sum array 
+
+# Fills prefix sum array 
+def fillPrefixSum(arr, n, prefixSum ):
+    
+    prefixSum[0] = arr[0]
+    
+    # Adding the present element 
+    # with previous element
+    for i in range(1, n):
+        prefixSum[i] = prefixSum[i-1] + arr[i]
+        
+        
+# Driver code 
+arr = [10, 4, 16, 20]
+n = len(arr)
+
+prefixSum = [0 for i in range(n+1)]
+
+fillPrefixSum(arr, n, prefixSum)
+
+for i in range(n):
+    print(prefixSum[i], " ", end='')
